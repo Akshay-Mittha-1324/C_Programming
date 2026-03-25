eaaee99c1367b15199644377d1dc29936775320b
Akshay-Mittha-1324 <akshay134007@gmail.com>
2026-03-25 05:57:01 +0000

    update

--- STDIO/standard_io.c
+++ STDIO/standard_io.c
@@ -145,20 +145,115 @@ int main()
 }
 #endif
 
-#if 1
+#if 0
 
 // ? Formatted I / O
 
 int main()
 {
-    char a[8] = "Hello";
-    printf(a);  // ? This will print the message 
-    printf("\n");
-    printf("%d\t%i\n",10,10);
-    printf("%e\t%E\n",1.2,1.2);
-    printf("%a\t%A\n",123.4,123.4);
-    printf("%hX\t%lX\t%llX\t%Lf\n",0XFFFFFFFF,0XFFFFFFFFl,0XFFFFFFFFFFFFFFFF,1.23456789L);
+    // char a[8] = "Hello";
+    // printf(a);  // ? This will print the message 
+    // printf("\n");
+    // printf("%d\t%i\n",10,10);
+    // printf("%e\t%E\n",1.2,1.2);
+    // printf("%a\t%A\n",123.4,123.4);
+    //printf("%hX\t%lX\t%llX\t%Lf\n",0XFFFFFFFF,0XFFFFFFFFl,0XFFFFFFFFFFFFFFFF,1.23456789L);     // ? Length Specifiers
+
+    printf("Width Specifiers\n");
+    printf("%3d\n%2d\n%d\n",1,1,1); // ? Width Specifiers
+    printf("%3d\n%3d\n%3d\n",10,10,10);
+    printf("%3d\n%3d\n%3d\n",100,100,100);
+    printf("%10s\n","Hello");
+    printf("%20s\n","Hello");
+    printf("%*d\n%*d\n%*d\n",1,1,2,1,3,1);
+
+    // ? Precision Specifiers
+    printf("Precision Specifiers\n");
+    printf("%3.1d\n%3.2d\n%3.3d\n",1,1,1);  // ? Fill the blocks after specifying the width and precision 
+    printf("%.3f\n%.5f\n%.2f\n",1.9,2.5,3.1023);    // ? Display the numbers till the .number using precision
+    printf("%12.8s\n","Hello World");   // ? Fill 8 blocks after width of number
+
+    // ? Flags Specifiers
+    printf("Flags Specifiers\n");
+    printf("%#x\n%#X\n%#x\n", 0xA, 0xA, 10);
+    printf("%#o\n", 8);
+    printf("%-3d %-3d\n", 1, 1);    // ? -3d here means left alignment
+    printf("%-3d %-3d\n", 10, 10);
+    printf("%-3d %-3d\n", 100, 100);
+    printf("% 3d\n", 100);
+    printf("% 3d\n", -100); // ? _ space 3d will print the space before printing the value
+
+    printf("Escape Sequences\n");
+    printf("Hello\nWorld\n");   // ? newline sequence
+    printf("Hello Emertxe\rWorld\n");   // ? Carriage Return 
+    printf("Hello\tWorld\n");   // ? Tab space
+    printf("Hello\bWorld\n");   // ? backspace
+    printf("Hello\vWorld\n");   // ? vertical tab
+    printf("Hello\fWorld\n");   // ? form feed
+    printf("Hello\eWorld\n");   // ? escape
+    printf("Hello\\World\n");   // ? to print slash
+    printf("\"Hello World\"\n");    // ? to print the double quotes
+    printf("\n80%%\n");
     return 0;
 }
 
+#endif
+
+#if 0
+
+// ? Loading screen
+
+int main()
+{
+    int i;
+    for(i = 1; i <= 100; i++)
+    {
+        printf("\rLoading [] %3d%%",i);
+        fflush(stdout);
+        sleep(1);
+    }
+    return 0;
+}
+
+#endif
+
+#if 0
+int main()
+{
+    int num1 = 123;
+    char ch = 'A';
+    float num2 = 12.345;
+    char string[] = "Hello World";
+    printf("%d %c %f %s\n",num1 , ch, num2, string);
+    printf("%+05d\n", num1);
+    printf("%.2f %.5s\n", num2, string);
+    return 0;
+}
+
+#endif
+#if 0
+int main()
+{
+    int ret;
+    char str[] = "Hello world";
+    ret = printf("%s\n",str);
+    printf("THe previous printf() printed %d chars\n",ret);
+    printf("%d\n",printf("Mumbai\n"));
+    printf("%d %d\n",printf("Emertxe\n"),printf("Vikas\n"));
+    return 0;
+}
+#endif
+
+#if 1
+int main()
+{
+    int num1 = 123;
+    char ch = 'A';
+    float num2 = 12.345;
+    char string1[] = "sprintf() Test";
+    char string2[100];
+    sprintf(string2, "%d %c %f %s\n",num1 , ch, num2, string1); // ? It is stored into the string2 buffer
+    printf("%s", string2);  // ? To print the buffer
+    return 0;
+}
 #endif
